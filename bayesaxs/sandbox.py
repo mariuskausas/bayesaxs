e2160ce3dffab60214e8511a685b8ac7be8ce39e
mariuskausas <kausas.m@gmail.com>
2019-01-25 18:04:08 +0000

    written initial bayesian model functions to perform overal all posible pairs of selected curves

--- bayesaxs/sandbox.py
+++ bayesaxs/sandbox.py
@@ -1,4 +1,5 @@
 from saxsCurve import *
+from saxsBayes import *
 import matplotlib.pyplot as plt
 
 analysis = Analysis()
@@ -11,9 +12,34 @@ analysis.cluster_fits()
 
 analysis.extract_representative_fits()
 
-print(analysis.get_fit_set())
+#print(analysis.get_fit_set())
 
 print(analysis.get_repfit())
 
 
+
+# Bayesian testing
+
+curve1 = analysis.get_repfit()[1]
+curve2 = analysis.get_repfit()[4]
+
+# curve1 = analysis.get_fit_set()[3]
+# curve2 = analysis.get_fit_set()[4]
+
+trace = run(curve1, curve2)
+
+w1, w2 = summarize(trace)
+
+print(pm.summary(trace).T)
+
+print(chi2(curve1.get_iq(),
+	 curve1.get_fit() * w1 + curve2.get_fit() * w2,
+	 curve1.get_sigma()))
+#
+# print(analysis.get_repfit())
+#
+# array = run_all(analysis.get_repfit())
+#
+# sns.heatmap(array, annot=True)
+# plt.show()
 
--- bayesaxs/saxsBayes.py
+++ bayesaxs/saxsBayes.py
@@ -5,19 +5,25 @@ import matplotlib.pyplot as plt
 
 from saxsCurve import *
 
+exp_curve = Curve("./data/HOIP_removedNaN.dat")
+
 analysis = Analysis()
 analysis.initialize_fits()
 
-curve1 = analysis.get_fit_set()[4]
-curve2 = analysis.get_fit_set()[13]
+#
+# curve1 = analysis.get_fit_set()[4]
+# curve2 = analysis.get_fit_set()[13]
 
 # Chi-square calculation
-def chi(exp, theor, error):
+
+
+def chi1(exp, theor, error):
 	chi_value = tt.tensor.sum(tt.tensor.power((exp - theor) / error, 2)) / (
 				exp.size - 1)  ## exp.size to be swapped with Shannon Channel
 	return tt.tensor.sum(chi_value)
 
-def _chi(exp, theor, error):
+
+def chi2(exp, theor, error):
 
 	"""
 	Calculate reduced chi squared.
@@ -33,90 +39,72 @@ def _chi(exp, theor, error):
 
 	return np.sum(chi)
 
-# print(_chi(curve1.get_iq(), curve1.get_fit(), curve1.get_sigma()))
-#
-# plt.plot(curve1.get_q()[5:350], curve1.get_logiq()[5:350], label="exp")
-# plt.plot(curve1.get_q()[5:350], curve1.get_logfit()[5:350], label="fit")
-# plt.plot(curve2.get_q(), curve2.get_logfit(), label="fit")
-# plt.show()
-#
-# w1=0.2
-# w2=0.8
-#
-# composite = curve1.get_fit()[5:350] * w1 + curve2.get_fit()[5:350] * w2
-#
-# print(composite)
 
-# chi_value = _chi(curve1.get_iq()[5:350], curve1.get_fit()[5:350], curve1.get_sigma()[5:350])
+def run(curve1, curve2):
 
-# print(chi_value)
+	with pm.Model() as model:
 
+		# Uniform variables
+		w1 = pm.Uniform("w1", 0, 1)
+		w2 = pm.Deterministic("w2", 1 - w1)
+		lam = pm.Uniform("lam", 0, 10)
 
+		# Preparing theoretical scattering data input
+		composite = curve1.get_fit() * w1 + curve2.get_fit() * w2
 
+		# Chi square value for composite vs experimental data
+		chi_value = chi1(curve1.get_iq()[5:500], composite[5:500], curve1.get_sigma()[5:500])
 
+		# Likelihood
+		target = pm.Exponential("Chi", lam=lam, observed=chi_value)
 
-# arr = np.linspace(0, 1, 51)
-# arr1 = np.ones((51))
-# arr2 = arr1 - arr
-# list_of_chi = []
-#
-# for i in range(arr1.size):
-#     composite = curve1.get_fit()[5:200] * arr1[i] + curve2.get_fit()[5:200] * arr2[i]
-#     list_of_chi.append(_chi(curve1.get_iq()[5:200], composite, curve1.get_sigma()[5:200]))
-#
-# index = list_of_chi.index(np.min(list_of_chi))
-# print("Min Chi^2 value:", np.min(list_of_chi))
-# print("w1:", arr[index])
-# print("w2:", arr2[index])
+		# Sample
+		step = pm.Metropolis()
+		trace = pm.sample(2000, step=step, njobs=1)
 
-
-
-
-
-
-with pm.Model() as model:
-
-	# Uniform variables
-	w1 = pm.Uniform("w1", 0, 1)
-	w2 = pm.Deterministic("w2", 1 - w1)
-	lam = pm.Uniform("lam", 0, 10)
-
-	# Preparing theoretical scattering data input
-	composite = curve1.get_fit() * w1 + curve2.get_fit() * w2
-
-	# Chi square value for composite vs experimental data
-	chi_value = chi(curve1.get_iq()[5:300], composite[5:300], curve1.get_sigma()[5:300])
-
-	# Likelihood
-	target = pm.Exponential("Chi", lam=lam, observed=chi_value)
-
-	# Sample
-	step = pm.Metropolis()
-	trace = pm.sample(2000, step=step, njobs=1)
-
-pm.plot_posterior(trace)
-plt.show()
-
-# summary = {}
-# varnames = trace.varnames
-#
-# for var in varnames:
-# 	_trace = trace[var]
-# 	mu = _trace.mean()
-# 	sd = _trace.std()
-#
-# 	summary[var] = dict(zip(['mu', 'sd'], [mu, sd]))
-#
-# print(summary)
-
-
+	return trace
 
 def summarize(trace):
 
-	summary = pm.summary(trace).T
+	summary = {}
+	varnames = trace.varnames
 
-	stats = ['mu', 'sd']
-	vars = summary.keys()
+	for var in varnames:
+		_trace = trace[var]
+		mu = _trace.mean()
+		sd = _trace.std()
+
+		summary[var] = dict(zip(['mu', 'sd'], [mu, sd]))
+
+	return summary['w1']['mu'], summary['w2']['mu']
+
+def run_all(curves):
+
+	n = len(curves)
+	array = np.zeros((n, n))
+	count = 1
+
+	for i in range(n):
+		for j in range(count, n):
+
+			curve1 = curves[i]
+			curve2 = curves[j]
+
+			trace = run(curve1, curve2)
+
+			w1, w2 = summarize(trace)
+
+			chi_value = chi2(curve1.get_iq(),
+							   (curve1.get_fit() * w1 + curve2.get_fit() * w2),
+							   curve1.get_sigma())
+
+			array[i][j] = chi_value
+
+		count += 1
+
+	return array
+
+
 
 
 
