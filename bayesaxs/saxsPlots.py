853aa260fe17a717961b10519a581d8032209a95
mariuskausas <kausas.m@gmail.com>
2019-02-18 17:41:33 +0000

    updated plotting function to display selected theoretical fits

--- bayesaxs/saxsPlots.py
+++ bayesaxs/saxsPlots.py
@@ -1,3 +1,5 @@
+from saxsCurve import *
+from saxsBayes import *
 import matplotlib.pyplot as plt
 
 def plot_saxsCurve(q, iq, fit=None, plot_fit=False):
@@ -11,11 +13,26 @@ def plot_saxsCurve(q, iq, fit=None, plot_fit=False):
 		plt.plot(q, iq)
 		plt.show()
 
+analysis = Analysis()
+
+analysis.initialize_fits()
+
+analysis.calc_pairwise_chi()
+
+analysis.cluster_fits()
+
+analysis.extract_representative_fits()
+
 
 colors = ["tab:blue", "tab:red", "tab:orange", "tab:brown", "tab:green", "tab:grey", "tab:olive"]
 curves = analysis.get_fit_set()
+curves1 = analysis.get_repfit()
 
-plt.figure(figsize=(10, 10))
+
+plt.figure(figsize=(10, 12.5))
+
+plt.subplot(211)
+
 for indx, cluster in enumerate(analysis.get_indices_of_clusterids()):
 	for curve in cluster:
 		plt.plot(curves[curve].get_q(),
@@ -23,23 +40,25 @@ for indx, cluster in enumerate(analysis.get_indices_of_clusterids()):
 				 label=str(curves[curve].get_title()),
 				 color=colors[indx],
 				 linewidth=2)
-plt.xlim(0, 0.15)
-plt.ylim(-2.75, -0.8)
+plt.xlim(0, 0.5)
+plt.ylim(-4, -0.8)
+plt.ylabel('$log_{10}$ $(I_{q})$', fontsize=20)
+plt.tick_params(labelsize=20)
 plt.legend()
-plt.savefig("cluster_fits.png", dpi=600)
-plt.show()
 
-curves1 = analysis.get_repfit()
+plt.subplot(212)
 
-plt.figure(figsize=(10, 10))
 for indx, curve in enumerate(curves1):
 		plt.plot(curves1[indx].get_q(),
 				 curves1[indx].get_logfit(),
 				 label=str(curves1[indx].get_title()),
 				 color=colors[indx],
 				 linewidth=2)
-plt.xlim(0, 0.15)
-plt.ylim(-2.75, -0.8)
+plt.xlim(0, 0.5)
+plt.ylim(-4, -0.8)
+plt.xlabel('$q$ $(\AA^{-1})$', fontsize=20)
+plt.ylabel('$log_{10}$ $(I_{q})$', fontsize=20)
+plt.tick_params(labelsize=20)
 plt.legend()
-plt.savefig("selected_cluster_fits.png", dpi=600)
+plt.savefig("selected_clusters.png", dpi=300)
 plt.show()
