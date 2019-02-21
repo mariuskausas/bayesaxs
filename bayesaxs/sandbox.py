1be2206af9c7081a78f417889131aed12e2f4e20
mariuskausas <kausas.m@gmail.com>
2019-02-21 17:09:15 +0000

    BayesianModel() and BayesianSampling() classes were written to perform basic multiple state inference problems

--- bayesaxs/sandbox.py
+++ bayesaxs/sandbox.py
@@ -1,56 +1,21 @@
 from saxsCurve import *
-import saxsBayes
 from saxsBayes import *
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-
-def _chi_tt(exp, theor, error):
-
-	"""
-	Calculate reduced chi squared (theano method).
-
-	"""
-
-	chi_value = tt.tensor.sum(tt.tensor.power((exp - theor) / error, 2)) / (
-				exp.size - 1)
-	return tt.tensor.sum(chi_value)
 
 
 # Initialize fits
 
 analysis = Analysis()
-
 analysis.initialize_fits()
-
 analysis.calc_pairwise_chi()
-
 analysis.cluster_fits()
-
 analysis.extract_representative_fits()
 
 print(analysis.get_repfit())
 
 
-
 # Bayesian testing
 
-# bayesmodel = BayesModel()
-#
-# bayesmodel.load_curves(analysis.get_repfit())
-
-# bayesmodel.run_multi_state_inference(states=4)
-
-# array = bayesmodel.run_two_state_inference()
-#
-# sns.heatmap(array, annot=True, fmt=".1f")
-# plt.savefig("multi_state.png", dpi=300)
-# plt.show()
-#plt.savefig("multi_state.png", dpi=600)
-
 sample = BayesSampling(analysis.get_repfit())
-sample.sample(step="metropolis", num_samples=50000)
-
-print(pm.summary(sample._trace))
+sample.inference_multiple(3, step="metropolis", num_samples=1000)
 
 
--- bayesaxs/saxsBayes.py
+++ bayesaxs/saxsBayes.py
@@ -1,16 +1,15 @@
 from itertools import combinations
-from six import string_types
 import numpy as np
 import pymc3 as pm
 import theano as tt
-import matplotlib.pyplot as plt
 
 from saxsCurve import *
+
 
 # Utilities
 
 
-def _chi_np(exp, theor, error):
+def _chi_np(exp, theor, sigma):
 
 	"""
 	Calculate reduced chi squared (numpy method).
@@ -20,60 +19,49 @@ def _chi_np(exp, theor, error):
 	# Catch division by zero errors. First do the division, then provide a zero array with the same size as the
 	# original array. Finish by populating zero array with values and skip those that had a zero in a denominator.
 
-	nominator = np.sum(np.power(np.divide((exp - theor), error, out=np.zeros_like(exp-theor), where=error != 0), 2))
+	nominator = np.sum(np.power(np.divide((exp - theor), sigma, out=np.zeros_like(exp-theor), where=sigma != 0), 2))
 	chi = np.divide(nominator, (exp.size - 1))
 
 	return np.sum(chi)
 
 
-def _chi_tt(exp, theor, error):
+def _chi2_tt(exp, theor, sigma):
+
+	"""
+	Calculate chi squared (theano method).
+
+	"""
+
+	chi_value = tt.tensor.sum(tt.tensor.power((exp - theor) / sigma, 2))
+	return tt.tensor.sum(chi_value)
+
+
+def _chi2red_tt(exp, theor, sigma):
 
 	"""
 	Calculate reduced chi squared (theano method).
 
 	"""
 
-	chi_value = tt.tensor.sum(tt.tensor.power((exp - theor) / error, 2)) / (
+	chi_value = tt.tensor.sum(tt.tensor.power((exp - theor) / sigma, 2)) / (
 				exp.size - 1)
 	return tt.tensor.sum(chi_value)
 
 
-def _chi_tt2(exp, theor, error):
-
-	"""
-	Calculate reduced chi squared (theano method).
-
-	"""
-
-	chi_value = tt.tensor.sum(tt.tensor.power((exp - theor) / error, 2))
-	return tt.tensor.sum(chi_value)
-
-
-def summarize(trace):
-
-		""" Simple summary function to return weights for a two state scattering model."""
-
-		# Prepare a dictionary to store trace information
-		summary = {}
-		varnames = trace.varnames
-
-		# For each trace variable extract mean and standard deviation
-		for var in varnames:
-			_trace = trace[var]
-			mu = _trace.mean()
-			sd = _trace.std()
-
-			summary[var] = dict(zip(['mu', 'sd'], [mu, sd]))
-
-		return summary['w1']['mu'], summary['w2']['mu']
-
-
 class BayesModel(object):
 
-	def __init__(self, title="BayesModel"):
+	def __init__(self, curves, title="BayesModel"):
 
 		self._title = title
-		self._curves = None
+		self._curves = curves
+		self._shape = len(curves)
+		self._exp_curve = self._curves[0].get_iq()
+		self._exp_sigma = self._curves[0].get_sigma()
+		self._model = pm.Model()
+		self._dirichlet = None
+		self._weighted_curve = None
+		self._chi2 = None
+		self._likelihood = None
 		self._trace = None
 
 	def __repr__(self):
@@ -104,173 +92,12 @@ class BayesModel(object):
 
 		return self._curves
 
-	def two_state_inference(self, curve1, curve2):
-
-		""" Bayesian inference of a two state scattering model."""
-
-		with pm.Model():
-
-			# Prepare weights
-			w1 = pm.Uniform("w1", 0, 1)
-			w2 = pm.Deterministic("w2", 1 - w1)
-			lam = pm.Uniform("lam", 0, 10)
-
-			# Prepare a weighted scattering curve
-			composite = curve1.get_fit() * w1 + curve2.get_fit() * w2
-
-			# Chi square value for composite vs experimental data
-			chi_value = _chi_tt(curve1.get_iq()[5:], composite[5:], curve1.get_sigma()[5:])
-
-			# Likelihood
-			target = pm.Exponential("Chi", lam=lam, observed=chi_value)
-
-			# Sample
-			step = pm.Metropolis()
-			self._trace = pm.sample(2000, step=step, njobs=1)
-
-			return BayesModelResults(self._trace).two_state_summary()
-
-	def multi_state_inference(self, curves):
-
-		with pm.Model():
-			num_curves = len(curves)
-
-			# Prepare weights
-			w = pm.Dirichlet("w", a=np.ones(num_curves), shape=num_curves)
-			lam = pm.Uniform("lam", 0, 10)
-
-			# Calculate composite curve
-			composite2 = np.sum([(curves[i].get_fit() * w[i]) for i in range(num_curves)], axis=0)
-			chi_value = _chi_tt(curves[0].get_iq()[5:], composite2[5:], curves[0].get_sigma()[5:])
-
-			# Likelihood
-			target = pm.Exponential("Chi", lam=lam, observed=chi_value)
-
-			# Sample
-			step = pm.Metropolis()
-			trace = pm.sample(2000, step=step, njobs=1)
-
-			return BayesModelResults(trace).multi_state_summary()
-
-	def run_two_state_inference(self):
-
-		""" Perform a pairwises Bayesian inference for all given curves."""
-
-		# Initialize an array
-		n = len(self._curves)
-		array = np.zeros((n, n))
-		count = 1
-
-		# Populate upper array triangle with inferred chi square values
-		for i in range(n):
-			# Variable count is used to populate an upper triangle
-			for j in range(count, n):
-
-				# Define two scattering curves
-				curve1 = self._curves[i]
-				curve2 = self._curves[j]
-
-				# Bayesian inference
-				w1, w2 = BayesModel.two_state_inference(self, curve1, curve2)
-
-				# Chi square value calculation
-				array[i][j] = _chi_np(curve1.get_iq(),
-							(curve1.get_fit() * w1 + curve2.get_fit() * w2),
-							curve1.get_sigma())
-
-			# Variable ount is used to populate an upper triangle
-			count += 1
-
-		return array
-
-	def run_multi_state_inference(self, states):
-
-		with open('multi_state{0}.txt'.format(str(states)), 'w') as f:
-
-				combs = combinations(self._curves, states)
-
-				for comb in combs:
-
-					# Bayesian inference
-					mu, sd = BayesModel.multi_state_inference(self, comb)
-
-					# Composite
-					composite = np.sum([(comb[i].get_fit() * mu[i]) for i in range(states)], axis=0)
-
-					# Chi value calculation
-					chi_value = _chi_np(comb[0].get_iq(),
-							composite,
-							comb[0].get_sigma())
-
-					print("-----------------------------", file=f)
-					print("Simulation", file=f)
-					print("                             ", file=f)
-					print("Chi square value: {}".format(np.round(chi_value, 3)), file=f)
-					for indx, curve in enumerate(comb):
-						print(curve, "mu: {} sd: {}".format(np.round(mu[indx], 3), np.round(sd[indx], 3)), file=f)
-					print("                             ", file=f)
-					print("-----------------------------", file=f)
-
-
-class BayesModelResults(object):
-
-	def __init__(self, trace):
-		self._trace = trace
-
-	def two_state_summary(self):
-
-		""" Simple summary function to return weights for a two state scattering model."""
-
-		# Prepare a dictionary to store trace information
-		summary = {}
-		varnames = self._trace.varnames
-
-		# For each trace variable extract mean and standard deviation
-		for var in varnames:
-			_trace = self._trace[var]
-			mu = _trace.mean()
-			sd = _trace.std()
-
-			summary[var] = dict(zip(['mu', 'sd'], [mu, sd]))
-
-		return summary['w1']['mu'], summary['w2']['mu']
-
-	def multi_state_summary(self):
-
-		""" Simple summary function to return weights and standard deviations for a multi state scattering model."""
-
-		mu = self._trace['w'].mean(axis=0)
-		sd = self._trace['w'].std(axis=0)
-
-		return mu, sd
-
-
-class BayesModel2(object):
+class BayesSampling(BayesModel):
 
 	def __init__(self, curves):
 
-		self._curves = curves
-		self._shape = len(curves)
-		self._exp_curve = self._curves[0].get_iq()
-		self._exp_sigma = self._curves[0].get_sigma()
-		self._model = pm.Model()
-		self._dirichlet = None
-		self._weighted_curve = None
-		self._chi2 = None
-		self._likelihood = None
-		self._trace = None
-
-
-class BayesSampling(BayesModel2):
-
-	def __init__(self, curves):
-
-		BayesModel2.__init__(self, curves)
+		BayesModel.__init__(self, curves)
 		self._initialize_variables()
-
-	@staticmethod
-	def _composite(curves, weights, shape):
-		return np.sum([(curves[i].get_fit() * weights[i]) for i in range(shape)], axis=0)
 
 	def _initialize_variables(self):
 
@@ -280,24 +107,79 @@ class BayesSampling(BayesModel2):
 			self._dirichlet = pm.Dirichlet("w", a=np.ones(self._shape), shape=self._shape)
 
 			# Calculate a weighted curve
-			self._weighted_curve = BayesSampling._composite(self._curves, self._dirichlet, self._shape)
-			self._chi2 = _chi_tt2(self._exp_curve[5:], self._weighted_curve[5:], self._exp_sigma[5:])
+			self._weighted_curve = BayesSampling._composite(curves=self._curves,
+															weights=self._dirichlet,
+															shape=self._shape)
+			self._chi2 = _chi2_tt(exp=self._exp_curve[5:],
+								theor=self._weighted_curve[5:],
+								sigma=self._exp_sigma[5:])
 
 			# Set likelihood in a form of exp(-chi2)
 			self._likelihood = pm.Exponential("lam", lam=1, observed=self._chi2)
 
-	def sample(self, step, num_samples, chains=1, njobs=1):
+	@staticmethod
+	def _composite(curves, weights, shape):
+
+		return np.sum([(curves[i].get_fit() * weights[i]) for i in range(shape)], axis=0)
+
+	def _inference(self, step, num_samples, chains=1, njobs=1):
 
 		with self._model:
 
 			# Set the MCMC sampler
-			if isinstance(step, string_types):
+			if isinstance(step, str):
 				step = {
 					'nuts': pm.NUTS(),
 					'metropolis': pm.Metropolis()
 				}[step.lower()]
 
 			self._trace = pm.sample(num_samples, step=step, chains=chains, njobs=njobs)
+
+	def _weight_summary(self):
+
+		""" Simple summary function to return weights and standard deviations for a multi state scattering model."""
+
+		mu = self._trace['w'].mean(axis=0)
+		sd = self._trace['w'].std(axis=0)
+
+		return mu, sd
+
+	def _summary_output(self, mu, sd, final_chi2):
+
+		print("-----------------------------")
+		print("Simulation")
+		print("                             ")
+		print("Chi square value: {}".format(np.round(final_chi2, 3)))
+		for indx, curve in enumerate(self._curves):
+			print(curve, "mu: {} sd: {}".format(np.round(mu[indx], 3), np.round(sd[indx], 3)))
+		print("                             ")
+		print("-----------------------------")
+
+	def _summary(self):
+
+		# Get the weights
+		mu, sd = BayesSampling._weight_summary(self)
+
+		# Calculate the weighted curve chi2red
+		final_weighted_curve = np.sum([(self._curves[i].get_fit() * mu[i]) for i in range(self._shape)], axis=0)
+		final_chi2 = _chi_np(exp=self._exp_curve, theor=final_weighted_curve, sigma=self._exp_sigma)
+
+		# Print the summary output
+		BayesSampling._summary_output(self, mu=mu, sd=sd, final_chi2=final_chi2)
+
+	@staticmethod
+	def _inference_single(curves, **kwargs):
+
+		single_state = BayesSampling(curves)
+		single_state._inference(**kwargs)
+		single_state._summary()
+
+	def inference_multiple(self, states, **kwargs):
+
+		combs = combinations(self._curves, states)
+		for comb in combs:
+			BayesSampling._inference_single(comb, **kwargs)
+
 
 
 
