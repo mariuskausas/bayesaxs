c507cc8ecf8ea646df9c093feca219e2d16d93f4
mariuskausas <kausas.m@gmail.com>
2019-10-11 10:33:56 +0100

    Updated bayesChi methods.
    Now there are two chi2 and chi2red calculations as both numpy and theano implementations

--- bayesaxs/bayesChi.py
+++ bayesaxs/bayesChi.py
@@ -2,33 +2,33 @@ import numpy as np
 import theano as tt
 
 
-def chi_np(exp, theor, sigma):
+def chi2_np(exp, theor, sigma):
+	""" Calculate chi squared (numpy method)."""
+	# Catch division by zero errors. First do the division, then provide a zero array with the same size as the
+	# original array. Finish by populating zero array with values and skip those that had a zero in a denominator.
+	chi2 = np.sum(np.power(np.divide((exp - theor), sigma, out=np.zeros_like(exp-theor), where=sigma != 0), 2))
+	return chi2
+
+
+def chi2red_np(exp, theor, sigma):
 	""" Calculate reduced chi squared (numpy method)."""
 	# Catch division by zero errors. First do the division, then provide a zero array with the same size as the
 	# original array. Finish by populating zero array with values and skip those that had a zero in a denominator.
 	nominator = np.sum(np.power(np.divide((exp - theor), sigma, out=np.zeros_like(exp-theor), where=sigma != 0), 2))
-	chi = np.divide(nominator, (exp.size - 1))
-	return np.sum(chi)
+	chi2red = np.divide(nominator, (exp.size - 1))
+	return chi2red
 
 
 def chi2_tt(exp, theor, sigma):
 	""" Calculate chi squared (theano method)."""
-	diff = exp - theor
-	chi_value = tt.tensor.sum(tt.tensor.power(tt.tensor.switch(tt.tensor.eq(diff, 0), 0, diff / sigma), 2))
-	return chi_value
-
-
-def chi2_tt2(exp, theor, sigma):
-	""" Calculate chi squared (theano method)."""
-	chi_value = tt.tensor.sum(tt.tensor.power((exp - theor) / sigma, 2))
-	return chi_value
+	chi2 = tt.tensor.sum(tt.tensor.power((exp - theor) / sigma, 2))
+	return chi2
 
 
 def chi2red_tt(exp, theor, sigma):
 	""" Calculate reduced chi squared (theano method)."""
-	chi_value = tt.tensor.sum(tt.tensor.power((exp - theor) / sigma, 2)) / (
-				exp.size - 1)
-	return chi_value
+	chi2red = tt.tensor.sum(tt.tensor.power((exp - theor) / sigma, 2)) / (exp.size - 1)
+	return chi2red
 
 
 def pairwise_chi(curves):
@@ -37,13 +37,14 @@ def pairwise_chi(curves):
 	:param curves: A list containing curves to iterate over.
 	:return: An array containing pairwise reduced chi squared values.
 	"""
+
 	# Generate an empty array (n,n) for a given n of curves
 	number_of_curves = len(curves)
 	pairwise_mat = np.zeros((number_of_curves, number_of_curves))
+
 	# Perform a pairwise reduced chi squared calculation
 	for i in range(number_of_curves):
 		for j in range(number_of_curves):
-			pairwise_mat[i:i + 1, j:j + 1] = chi_np(curves[i].get_fit(),
-																curves[j].get_fit(),
-																curves[i].get_sigma())
+			pairwise_mat[i:i + 1, j:j + 1] = chi2red_np(curves[i].get_fit(), curves[j].get_fit(), curves[i].get_sigma())
+
 	return pairwise_mat
