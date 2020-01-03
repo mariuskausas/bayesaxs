979304b6e107f064770d1143b59b060bfeaa9c61
mariuskausas <kausas.m@gmail.com>
2020-01-03 20:56:39 +0000

    Adding documentation for HDBSCAN class.

--- bayesaxs/cluster/hdbscan.py
+++ bayesaxs/cluster/hdbscan.py
@@ -10,6 +10,16 @@ class HDBSCAN(BaseCluster):
 
 	The HDBSCAN object allows to perform a clustering on a given trajectory.
 
+	Parameters
+	----------
+	min_cluster_size : int
+		The minimum size of clusters.
+	metric : str
+		The metric to use when calculating distance between instances in a feature array.
+	core_dist_n_jobs : int
+		Number of parallel jobs to run in core distance computations.
+		For core_dist_n_jobs below -1, (n_cpus + 1 + core_dist_n_jobs) are used.
+
 	Attributes
 	----------
 	clusterer : hdbscan.hdbscan_.HDBSCAN object
