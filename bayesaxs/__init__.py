4c8f309453c1c759cd041566108b37d408ca5d57
Marius <22818740+mariuskausas@users.noreply.github.com>
2024-08-23 09:26:36 +0100

    Update __init__.py

--- bayesaxs/__init__.py
+++ bayesaxs/__init__.py
@@ -3,18 +3,25 @@ bayesaxs
 Python implementation of BSS-SAXS
 """
 
-# Handle versioneer
-from ._version import get_versions
-versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
-del get_versions, versions
-
-__all__ = ["basis", "cluster", "inference", "viz", "utils"]
-
 from bayesaxs.basis import Curve, Scatter
 from bayesaxs.cluster import Agglomerative, HDBSCAN, KMeans
-from bayesaxs.inference.sampler import Sampler
-from bayesaxs.viz.plotting import plot_single_fit, plot_multiple_fits, plot_dendogram
-from bayesaxs.viz.plotting import plot_clusters_vs_scatters, plot_weights
-from bayesaxs.utils.utils import load_pickle, save_pickle
+from bayesaxs.inference import Sampler
+from bayesaxs.viz import plot_single_fit, plot_multiple_fits, plot_dendogram
+from bayesaxs.viz import plot_clusters_vs_scatters, plot_weights
+from bayesaxs.utils import load_pickle, save_pickle
+
+__all__ = [
+    "Curve", 
+    "Scatter", 
+    "Agglomerative", 
+    "HDBSCAN", 
+    "KMeans",
+    "Sampler",
+    "plot_single_fit", 
+    "plot_multiple_fits", 
+    "plot_dendogram", 
+    "plot_clusters_vs_scatters", 
+    "plot_weights",
+    "load_pickle",
+    "save_pickle"
+    ]
